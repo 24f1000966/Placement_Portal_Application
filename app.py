34c101aea848d37d72e81a6083d0c9de789b3989
Jayshree Jena <jenajayshree2005@gmail.com>
2026-03-27 12:28:00 +0530

    updated localport

--- app.py
+++ app.py
@@ -515,5 +515,5 @@ def init_db():
 if __name__ == '__main__':
     if not os.path.exists('placement.db'):
         init_db()
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
 
